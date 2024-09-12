ee89415e520b39cf70655f2f62f55eb9b2890b06
Shalom Raichman <shalomr1770@gmail.com>
2024-09-12 09:50:44 +0300

    mission 2 completed

--- exam.js
+++ exam.js
@@ -1,7 +1,13 @@
-const arr = [1, 8, 7 , 6]
 
 const Mission1 = (arr) => {
     return arr.filter(n => n % 2 == 0)
+}
+
+const Mission2 = (str) => {
+    const wordLength = 4
+    let count = 0
+    const strArr = str.split(" ").filter(str => str.length == wordLength)
+    return strArr.length
 }
 
 
